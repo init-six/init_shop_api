aac22bfa6b969684a64087da6313f77eb1812d20
Evan-LZM <zliu246@aucklanduni.ac.nz>
2021-12-09 23:18:03 +1300

    Move Category/Product Add Dto To Models
    Move category/product add dto to models

--- Models/CategoryAddDto.cs
+++ Models/CategoryAddDto.cs
@@ -1,9 +1,10 @@
 using System;
 
-namespace init_api.Entities
+namespace init_api.Models
 {
     public class CategoryAddDto{
         public string Name{get;set;}
         public string Icon {get;set;}
+        public ICollection<ProductAddDto> Products {get;set;} = new List<ProductAddDto>();
     }
 }
